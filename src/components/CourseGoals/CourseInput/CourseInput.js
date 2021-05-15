e84b0b4676a5c91927392d8c7cd06887bbe6b80b
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-15 22:13:51 +0200

    Remove the ability to add an empty task

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,7 +8,9 @@ const CourseInput = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (enteredValue.length) {
+      props.onAddGoal(enteredValue);
+    }
     setEnteredValue("");
   };
 
