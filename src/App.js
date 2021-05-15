81243d831a6683781fa6af4ca645e6a0af2b5e30
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-15 22:09:48 +0200

    Create a data entry form and view added tasks to the task list.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
 
 function App() {
+  const INITIAL_GOALS = [
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
+  ];
+  const [courseGoals, setCourseGoals] = useState(INITIAL_GOALS);
+
+  const addGoalHandler = (entryText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: entryText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalID) => {
+    setCourseGoals((prevGoals) => {
+      return prevGoals.filter((goal) => goal.id !== goalID);
+    });
+    console.log(goalID);
+  };
+
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one? :)</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+
+      <section id="goals">{content}</section>
     </div>
   );
 }
